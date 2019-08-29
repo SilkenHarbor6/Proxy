6da9c4ef793708a14169461ec76743dd41bf2d68
Bryan Palma <bryan.palmafgksa@sv.cds>
2019-08-29 08:48:41 -0600

    Reparacion de save

---
+++ Proxy/Proxy/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy.ViewModel
+{
+    using System.ComponentModel;
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
